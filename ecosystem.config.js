3875879adc81fdbf4d3b13fd4a9a83430b105ca9
Josh Walshaw <joshscottmain@gmail.com>
2020-11-25 18:59:49 +0000

    adding pm2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [
+      {
+        name: 'Cloudflare DNS Updater',
+        script: './dist/Application.js',
+      }
+  ],
+  deploy : {
+    production : {
+      user : 'josh',
+      host : '192.168.0.40',
+      ref  : 'origin/master',
+      repo : 'git@gitlab.com:walshaw-home/cloudflare-updater.git',
+      path : '/home/josh/cloudflare-updater',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && tsc && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
