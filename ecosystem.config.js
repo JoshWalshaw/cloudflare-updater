eeb0325bbd5a4cba65a3cd18bc5eb467fc7941f1
Josh Walshaw <joshscottmain@gmail.com>
2020-11-26 20:36:01 +0000

    Updating the deployment host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,14 @@ module.exports = {
   ],
   deploy : {
     production : {
-      user : 'josh',
-      host : '192.168.0.40',
-      port : '6063',
-      ref  : 'origin/master',
-      repo : 'git@gitlab.com:walshaw-home/cloudflare-updater.git',
-      path : '/home/josh/cloudflare-updater',
+      user: 'josh',
+      host: 'developer.joshwalshaw.com',
+      port: '6063',
+      ref: 'origin/master',
+      repo: 'git@gitlab.com:walshaw-home/cloudflare-updater.git',
+      path: '/home/josh/cloudflare-updater',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && tsc && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && tsc && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
